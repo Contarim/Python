d86ab23b8b1bc113dd1de16a9813bf476e5c7b7e
João Contarim <joaocontar@hotmail.com>
2025-03-12 18:56:21 -0300

    Exercicios de .Format e Types

--- Python-atv-6.py
+++ Python-atv-6.py
@@ -1,4 +1,6 @@
 a=int(input('Digite o primeiro numero: '))
 b=int(input('Digite o segundo numero: '))
 
-print ('A soma do primeiro com o segundo é:', (a+b));
+#print ('A soma do primeiro com o segundo é:', (a+b));
+
+print('A soma entre {} e {} é de {}'.format(a, b, a+b));
---
+++ Python-atv-7.py
@@ -0,0 +1,9 @@
+n=input('Digite algo: ')
+
+print('O tipo da variável é:',type(n));
+print('Está tudo em maiúsculo? ',n.isupper());
+print('Está tudo em minúsculo? ',n.islower());
+print('Contém apenas letras? ',n.isalpha());
+print('Contém apenas números? ',n.isnumeric());
+print('Contém letras e números? ',n.isalnum());
+print('Estpa capitalizada? ',n.istitle());
