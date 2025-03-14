f91dbbaa138039107c756d4bf61d31aa9524a66e
João Contarim <joaocontar@hotmail.com>
2025-03-13 23:45:12 -0300

    Novas atividades e anotações

--- Python-atv-4.py
+++ Python-atv-4.py
@@ -1,3 +1,6 @@
 nome=input('Qual o seu nome? ')
 
-print('Olá',nome,'prazer em ter você aqui!')
+print('Prazer em te conhecer {:<20}!\n'.format(nome))
+print('Prazer em te conhecer {:>20}!\n'.format(nome))
+print('Prazer em te conhecer {:^20}!\n'.format(nome))
+print('Prazer em te conhecer {:*^20}!\n'.format(nome))
