e4e79e40ec712c853098ac300eb18cd309fd3385
João Contarim <joaocontar@hotmail.com>
2025-03-16 08:48:18 -0300

    Adição dos exercicios 9,10 e 11

---
+++ Python-atv-11.py
@@ -0,0 +1,5 @@
+n1=float(input('Digite sua nota de Matemática do primeiro Bimestre: '))
+n2=float(input('Digite sua nota de Matemática do segundo Bimestre: '))
+m= (n1 + n2) / 2
+
+print('Você tirou {} no primeiro bimestre e {} no segundo bimestre, a sua média é {}'.format(n1,n2,m))
