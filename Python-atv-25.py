bda5fbe4b747319fb60d06e8d61bbd4a433f7409
João Contarim <joaocontar@hotmail.com>
2025-03-21 11:21:42 -0300

    Calculando seno, cosseno e tangente utilizando as funções Radians, Sin, Cos e Tan da biblioteca MATH

--- Python-atv-25.py
+++ Python-atv-25.py
@@ -1,17 +1,17 @@
-import math
+from math import radians,sin,cos,tan
 
 # Calculando o "Seno", "Cosseno" e "Tangente" a partir de um ângulo informado
 
 angulo = float(input('Digite um ângulo: '))
 
 # Radians converte o valor do ângulo para graus radianos
-angulo_rad = math.radians(angulo)
+
+seno = sin(radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
 
 # "sin", "cos" e "tan" calculam o valor convertido em graus radianos para encontrar o Seno, Cosseno e Tangente
-seno = math.sin(angulo_rad)
-cosseno = math.cos(angulo_rad)
-tangente = math.tan(angulo_rad)
 
-print('Seno : {:.2f}'.format(seno))
-print('Cosseno : {:.2f}'.format(cosseno))
-print('Tangente : {:.2f}'.format(tangente))
+print('O ângulo {} tem o Seno de {:.2f}'.format(angulo,seno))
+print('O ângulo {} tem o Cosseno : {:.2f}'.format(angulo,cosseno))
+print('O ângulo {} tem o Tangente : {:.2f}'.format(angulo,tangente))
